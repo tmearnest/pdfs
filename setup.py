36c0a0ac425bd1871bac2b1628faaba01d2aa32e
Tyler Earnest <tylere@rne.st>
2017-08-04 22:56:53 -0500

    simplified setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
-import glob, os, subprocess, json, time, getpass, socket
-from setuptools import setup
-import setuptools.command.build_py as duc
+from setuptools import setup, find_packages
 
-pkgName = 'sbd'
-ver = '0.0.3'
-description='Simple bibliography manager'
-long_description=open("README.md").read().strip()
-
-setup(name=pkgName,
-      version=ver,
-      cmdclass={'build_py': duc.build_py},
-      packages=['sbd', 'sbd.Commands'],
-      description=description,
-      long_description=long_description,
+setup(name='sbd',
+      version='0.0.3',
+      packages=find_packages(),
+      description='Simple bibliography manager',
+      long_description=open("README.md").read().strip(),
       author='Tyler M. Earnest',
-      data_files=[('sbd/templates', glob.glob("sbd/templates/*"))],
       author_email='tylere@rne.st',
-      url='http://tyler.ea.rne.st',
+      url='https://github.com/tmearnest/sbd',
       license='MIT License',
       entry_points = {"console_scripts": ['sbd = sbd.__main__:main']},
-      install_requires = ["prompt_toolkit", "inotify", "flask", "termcolor", "requests", "unidecode", "pdfminer.six", "pygments"],
-      zip_safe=False)
+      install_requires = ["flask", "inotify", "pdfminer.six",
+                          "prompt_toolkit", "pygments", "requests",
+                          "termcolor", "unidecode"])
